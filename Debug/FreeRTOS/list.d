7429d7f022249d23746b1222a74562f5d98c1125
TsenHill <chou870808@gmail.com>
2021-04-28 16:06:52 +0800

    first commit

---
+++ Debug/FreeRTOS/list.d
@@ -0,0 +1,25 @@
+FreeRTOS/list.o: ../FreeRTOS/list.c \
+ /Users/tsen/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab3/FreeRTOS/include/FreeRTOS.h \
+ /Users/tsen/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab3/FreeRTOS/include/FreeRTOSConfig.h \
+ /Users/tsen/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab3/FreeRTOS/include/projdefs.h \
+ /Users/tsen/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab3/FreeRTOS/include/portable.h \
+ /Users/tsen/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab3/FreeRTOS/include/deprecated_definitions.h \
+ /Users/tsen/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab3/FreeRTOS/portable/ARM_CM4F/portmacro.h \
+ /Users/tsen/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab3/FreeRTOS/include/mpu_wrappers.h \
+ /Users/tsen/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab3/FreeRTOS/include/list.h
+
+/Users/tsen/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab3/FreeRTOS/include/FreeRTOS.h:
+
+/Users/tsen/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab3/FreeRTOS/include/FreeRTOSConfig.h:
+
+/Users/tsen/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab3/FreeRTOS/include/projdefs.h:
+
+/Users/tsen/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab3/FreeRTOS/include/portable.h:
+
+/Users/tsen/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab3/FreeRTOS/include/deprecated_definitions.h:
+
+/Users/tsen/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab3/FreeRTOS/portable/ARM_CM4F/portmacro.h:
+
+/Users/tsen/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab3/FreeRTOS/include/mpu_wrappers.h:
+
+/Users/tsen/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab3/FreeRTOS/include/list.h:
